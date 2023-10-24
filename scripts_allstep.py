228960a7d13cf36df45b5fdd847a591ade5fdf22
lc451574367 <39795448+lc451574367@users.noreply.github.com>
2023-10-24 16:22:38 +0800

    Update scripts_allstep.py

--- scripts_allstep.py
+++ scripts_allstep.py
@@ -9,7 +9,23 @@ from get_vector import *
 from textprocess import *
 from gensim.scripts.glove2word2vec import glove2word2vec
 
-"Transfer format"
+"1. Download pre-trained models"
+"""
+Glove:https://nlp.stanford.edu/projects/glove/
+word2vec:https://code.google.com/archive/p/word2vec/
+bert:https://github.com/google-research/bert
+
+This tool provide some bert models download from official website:
+    uncased_L-2_H-128_A-2
+    uncased_L-4_H-128_A-2
+    uncased_L-6_H-128_A-2
+    uncased_L-8_H-128_A-2
+    uncased_L-10_H-128_A-2
+
+The other models are not provided due to the upload size limitation, if you want to try more different parameters, please download other models from the official website above.
+"""
+
+"2. Transfer format"
 """
 If you download the glove pre-trained vector from the official website,
 you should first convert the format to word2vec, then you will use the glove vector after format coversion to get embedding vectors
@@ -18,6 +34,10 @@ glove_model = 'models/glove/glove.6B.50d.txt'
 g2w_glove_model = 'models/glove/glove.6B.50d.g2w.txt'
 glove2word2vec(glove_model, g2w_glove_model)
 
+"3. Get embedding vector"
+"""
+After download the models, you can get different embedding vectors by different parameters.
+"""
 "1. word embedding vector"
 wordpath = 'test/test.txt'
 Word_vector(wordpath, modeltype='glove', corpus='common', dimension=300)
@@ -25,6 +45,6 @@ Word_vector(wordpath, modeltype='word2vec', corpus='GoogleNews')
 
 "2. sentence embedding vector"
 sentencepath = 'test/sentence.txt'
-Sentence_vector(sentencepath, modeltype = 'bert', layer = 12, dimension=768) 
+Sentence_vector(sentencepath, modeltype = 'bert', layer = 10, dimension=128) 
 Sentence_vector(sentencepath, modeltype = 'glove')
 Sentence_vector(sentencepath, modeltype = 'word2vec')
